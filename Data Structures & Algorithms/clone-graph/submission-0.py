391fe741f286e4f5712e8c540d083a4e647ac235
Nhat Minh <149989478+nhatminh-115@users.noreply.github.com>
2026-08-17 22:55:35 +0700

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        OldtoNew = {}
+
+        def dfs(node):
+            if node in OldtoNew:
+                return OldtoNew[node] #tránh dup, kiểu nhiều node cùng tới 1 node.
+            
+            copy = Node(node.val)
+            OldtoNew[node] = copy
+
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            
+            return copy
+        
+        if not node:
+            return None
+        
+        return dfs(node)
